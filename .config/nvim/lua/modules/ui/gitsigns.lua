4b2c1f58053f210cdb26f2e9e554fb3daf38bce4
Balazs Szabo <balazs.s@investing.com>
2021-11-16 10:27:51 +0200

    mapping change

--- .config/nvim/lua/modules/ui/gitsigns.lua
+++ .config/nvim/lua/modules/ui/gitsigns.lua
@@ -24,11 +24,11 @@ config.setup = function()
         ['n ]g'] = { expr = true, "&diff ? ']g' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'"},
         ['n [g'] = { expr = true, "&diff ? '[g' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"},
 
-        ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-        ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-        ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-        ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-        ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
+        -- ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+        -- ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+        -- ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+        -- ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+        -- ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
 
         -- Text objects
         ['o ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>',
