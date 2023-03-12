fe5d42c7fd6445132caa7d2f8c26d467cd8e3cde
Balázs Szabó <hobord@users.noreply.github.com>
2023-03-12 09:25:28 +0200

    automaton

--- neovim/.config/nvim/lua/modules/init.lua
+++ neovim/.config/nvim/lua/modules/init.lua
@@ -541,6 +541,44 @@ require('packer').startup(function(use)
   -- Tools
   --
 
+
+  use {
+    "Dax89/automaton.nvim",
+    requires = {
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope.nvim" },
+      { "mfussenegger/nvim-dap" }, -- Debug support for 'launch' configurations (Optional)
+      { "hrsh7th/nvim-cmp" }, -- Autocompletion for automaton workspace files (Optional)
+      { "L3MON4D3/LuaSnip" }, -- Snippet support for automaton workspace files (Optional)
+    },
+    config = function()
+      require("automaton").setup({
+        debug = false,
+        saveall = true,
+        ignore_ft = {},
+
+        integrations = {
+          luasnip = false,
+          cmp = false,
+        },
+
+        icons = {
+          buffer = "",
+          close = "",
+          launch = "",
+          task = "",
+          workspace = "",
+        },
+
+        events = {
+          workspacechanged = function(ws)
+            -- "ws" is the current workspace object (can be nil)
+          end
+        }
+      })
+    end
+  }
+
   -- use 'voldikss/vim-floaterm'
   -- use {
   --   'numtostr/FTerm.nvim',
